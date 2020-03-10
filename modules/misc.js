a8e29bfdb21b197304dcae011d37fc044de65b6b
Jyles Coad-Ward <jyles@dariox.club>
2020-03-10 12:51:15 +0800

    DO NOT UPDATE, Still in Devel for moving everything to module-based

--- modules/misc.js
+++ modules/misc.js
@@ -20,3 +20,52 @@ module.exports.writeFile = async function(location, content){
 	});
   return true;
 }
+
+module.exports.startPUBGNode = function(config, argv) {
+	if (config.api_key === "not_set") {
+		require('./cmd.js').prog_setup();
+	} else {
+		switch (argv[2].split("\r").join("")) {
+			case '-m':
+			case '--export-match-data':
+				require('./cmd.js').getmatchdata();
+				break;
+			case '-mu':
+			case '--latest-match-from-username':
+				require('./cmd.js').getlatestmatchfromusername();
+				break;
+			case '-mhtml':
+			case '--match-export-to-html':
+				require('./cmd.js').exportashtml();
+				break;
+			case '-muhtml':
+			case '--match-export-to-html-from-username':
+				require('./cmd.js').exportashtmlfromuname();
+				break;
+			case '-p':
+			case '--get-player-info':
+				require('./cmd.js').getplayerinfo();
+				break;
+			case '-lm':
+			case '--get-latest-matchid-from-username':
+				require('./cmd.js').latestmatchidfromusername();
+				break;
+			case '-c':
+			case '--cleanup':
+				require('./cmd.js').cleanup();
+				break;
+			case '-s':
+			case '--setup':
+				require('./cmd.js').setup();
+				break;
+			case '-h':
+			case '--help':
+				require('./cmd.js').help();
+				break;
+			default:
+				console.log("No Valid Arguments Recognised, Try './tool --help' or 'node require('./cmd.js').js --help'");
+				process.exit();
+				break;
+		}
+	}
+}
